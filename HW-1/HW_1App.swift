8d76f959a9aff64362c3cec55e27c7d2d60625a0
amity871028 <amity3744039@gmail.com>
2020-10-06 16:34:52 +0800

    Initial Commit

---
+++ HW-1/HW_1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_1App.swift
+//  HW-1
+//
+//  Created by User04 on 2020/10/6.
+//
+
+import SwiftUI
+
+@main
+struct HW_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
